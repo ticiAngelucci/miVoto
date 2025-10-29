7c49089cd8ffb28c8a05457956b828c28b7fa565
Ignacio Juri <ignaciojuri1@gmail.com>
2025-10-29 20:49:13 -0300

    Creacion de carpetas backend

---
+++ backend/src/main/java/com/example/mivoto/MivotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mivoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MivotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MivotoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/mivoto/MivotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mivoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MivotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
